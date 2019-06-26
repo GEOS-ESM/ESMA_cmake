2499ae002c44419c47fb08f6c2bbe28b4e3b9c8c
Tom Clune <thomas.l.clune@nasa.gov>
2019-06-26 09:08:05 -0400

    Updating to include ecbuild within esma cmake.

---
+++ esma.cmake
@@ -0,0 +1,38 @@
+# Bring in ecbuild
+list (APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/@ecbuild/cmake")
+include (ecbuild_system NO_POLICY_SCOPE)
+
+list (APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/check_compiler_support")
+include ("${CMAKE_Fortran_COMPILER_ID}")
+include (check_fortran_support)
+include (esma_check_if_debug)
+include (esma_set_this)
+include (esma_add_subdirectories)
+include (esma_add_library)
+include (esma_generate_automatic_code)
+include (esma_create_stub_component)
+include (esma_fortran_generator_list)
+
+include (UseProTeX)
+set (protex_flags -g -b -f)
+
+set (LATEX_COMPILER pdflatex)
+include (UseLatex)
+
+
+if (APPLE)
+  include(osx_extras)
+endif ()
+
+
+# OpenMP support
+find_package (OpenMP)
+
+# Threading support
+set(CMAKE_THREAD_PREFER_PTHREAD TRUE)
+set(THREADS_PREFER_PTHREAD_FLAG TRUE)
+find_package(Threads REQUIRED)
+
+# Position independent code
+set(CMAKE_POSITION_INDEPENDENT_CODE ON)
+
