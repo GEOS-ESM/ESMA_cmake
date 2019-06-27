acf208a8b63aa0a2abc72f8851ea84fdfee0015a
Matthew Thompson <matthew.thompson@nasa.gov>
2019-06-27 15:06:03 -0400

    Updates from make flag matching campaign on 2019Jun27

--- GNU.cmake
+++ GNU.cmake
@@ -2,20 +2,45 @@ if (CMAKE_Fortran_COMPILER_VERSION VERSION_LESS 5.3)
   message(FATAL_ERROR "${CMAKE_Fortran_COMPILER_ID} version must be at least 5.3!")
 endif()
 
+set (FOPT0 "-O0")
+set (FOPT1 "-O1")
+set (FOPT2 "-O2")
+set (FOPT3 "-O3")
+set (FOPT4 "-O3")
+set (DEBINFO "-g")
+
+set (FPE0 "")
+set (FPE3 "")
+set (FP_MODEL_SOURCE "")
+set (FP_MODEL_STRICT "")
+set (FP_MODEL_CONSISTENT "")
+
+set (OPTREPORT0 "")
+set (OPTREPORT5 "")
+
 set (FREAL8 "-fdefault-real-8 -fdefault-double-8")
 set (FINT8 "-fdefault-integer-8")
+
 set (PP    "-cpp")
 set (MISMATCH "")
-# This doesn't seem to work at the moment
-#set (BIG_ENDIAN "-fconvert=swap")
+#set (BIG_ENDIAN "-fconvert=swap") # This doesn't seem to work at the moment
 set (EXTENDED_SOURCE "-ffixed-line-length-132")
 set (FIXED_SOURCE "-ffixed-form")
-set (DISABLE_FIELD_WIDTH_WARNING)
+set (DISABLE_FIELD_WIDTH_WARNING "")
 set (CRAY_POINTER "-fcray-pointer")
-set (ALIGNCOM "-falign-commons")
+set (MCMODEL "")
+set (HEAPARRAYS "")
 set (BYTERECLEN "-frecord-marker=4")
-set (NO_RANGE_CHECK "-fno-range-check")
+set (ALIGNCOM "-falign-commons")
 set (TRACEBACK "-fbacktrace")
+set (NOOLD_MAXMINLOC "")
+set (REALLOC_LHS "")
+set (ARCH_CONSISTENCY "")
+set (FTZ "")
+set (ALIGN_ALL "")
+set (NO_ALIAS "")
+
+set (NO_RANGE_CHECK "-fno-range-check")
 
 ####################################################
 
@@ -28,12 +53,12 @@ set (common_Fortran_fpe_flags "-ffpe-trap=zero,overflow ${TRACEBACK}")
 
 # GEOS Debug
 # ----------
-set (GEOS_Fortran_Debug_Flags "-O0 -g -fcheck=all,no-array-temps -finit-real=snan")
+set (GEOS_Fortran_Debug_Flags "${FOPT0} ${DEBINFO} -fcheck=all,no-array-temps -finit-real=snan")
 set (GEOS_Fortran_Debug_FPE_Flags "${common_Fortran_fpe_flags}")
 
 # GEOS Release
 # ------------
-set (GEOS_Fortran_Release_Flags "-O3 -march=westmere -mtune=generic -funroll-loops -g")
+set (GEOS_Fortran_Release_Flags "${FOPT3} -march=westmere -mtune=generic -funroll-loops ${DEBINFO}")
 set (GEOS_Fortran_Release_FPE_Flags "${common_Fortran_fpe_flags}")
 
 # GEOS Vectorize
@@ -42,11 +67,11 @@ set (GEOS_Fortran_Release_FPE_Flags "${common_Fortran_fpe_flags}")
 #       does not layout regress. Options kept here for testing purposes
 
 # Options per Jerry DeLisle on GCC Fortran List
-#set (GEOS_Fortran_Vect_Flags "-O2 -march=native -ffast-math -ftree-vectorize -funroll-loops --param max-unroll-times=4 -mprefer-avx128 -mno-fma")
+#set (GEOS_Fortran_Vect_Flags "${FOPT2} -march=native -ffast-math -ftree-vectorize -funroll-loops --param max-unroll-times=4 -mprefer-avx128 -mno-fma")
 
 # Options per Jerry DeLisle on GCC Fortran List with SVML
 #set (GEOS_Fortran_Vect_Flags "-O2 -march=native -ffast-math -ftree-vectorize -funroll-loops --param max-unroll-times=4 -mprefer-avx128 -mno-fma -mveclibabi=svml")
-#set (GEOS_Fortran_Vect_FPE_Flags "-g ${TRACEBACK}")
+#set (GEOS_Fortran_Vect_FPE_Flags "${DEBINFO} ${TRACEBACK}")
 
 # Until good options can be found, make vectorize equal common flags
 set (GEOS_Fortran_Vect_Flags ${GEOS_Fortran_Release_Flags})
