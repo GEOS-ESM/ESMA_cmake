2616db74aa3ea39fa22d9782d69741b781a080e1
Matthew Thompson <matthew.thompson@nasa.gov>
2019-11-14 12:32:49 -0500

    Add a FINDLOC() test

--- check_compiler_support/check_fortran_support.cmake
+++ check_compiler_support/check_fortran_support.cmake
@@ -4,3 +4,7 @@ try_fortran_compile(
   ${CMAKE_CURRENT_LIST_DIR}/assumed_type.F90
   FORTRAN_COMPILER_SUPPORTS_ASSUMED_TYPE
   )
+try_fortran_compile(
+  ${CMAKE_CURRENT_LIST_DIR}/findloc.F90
+  FORTRAN_COMPILER_SUPPORTS_FINDLOC
+  )
