cca5310e74780f5efea8988cc8c5d6ed436a354c
Liam Bindle <liambindle@gmail.com>
2020-02-13 13:33:41 -0600

    Use NATIVE_COMMAND in separate_arguments()

--- GenericCompiler.cmake
+++ GenericCompiler.cmake
@@ -3,5 +3,5 @@ set (GEOS_Fortran_FLAGS_RELEASE "${GEOS_Fortran_Release_Flags} ${common_Fortran_
 set (GEOS_Fortran_FLAGS_VECT    "${GEOS_Fortran_Vect_Flags} ${common_Fortran_flags} ${GEOS_Fortran_Vect_FPE_Flags} ${ALIGNCOM}")
 
 # Use separate_arguments to split options into semicolon separated list
-separate_arguments(GEOS_Fortran_FLAGS_RELEASE UNIX_COMMAND ${GEOS_Fortran_FLAGS_RELEASE})
-separate_arguments(GEOS_Fortran_FLAGS_DEBUG UNIX_COMMAND ${GEOS_Fortran_FLAGS_DEBUG})
+separate_arguments(GEOS_Fortran_FLAGS_RELEASE NATIVE_COMMAND ${GEOS_Fortran_FLAGS_RELEASE})
+separate_arguments(GEOS_Fortran_FLAGS_DEBUG NATIVE_COMMAND ${GEOS_Fortran_FLAGS_DEBUG})
