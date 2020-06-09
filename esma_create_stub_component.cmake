795287fe78815e21dc132a591855ebbeb7b19ad0
Tom Clune <thomas.l.clune@nasa.gov>
2020-06-09 12:42:32 -0400

    Fix for namespace collision.
    Previous use of macro() resulted in "generator" having an external
    value and then skipping the find_file().  Using function(),
    "generator" is a local variable and properly filled.

--- esma_create_stub_component.cmake
+++ esma_create_stub_component.cmake
@@ -1,8 +1,10 @@
-macro (esma_create_stub_component srcs module)
+function (esma_create_stub_component srcs module)
   list (APPEND ${srcs} ${module}.F90)
+
   find_file (generator
     NAME mapl_stub.pl
     PATHS ${MAPL_SOURCE_DIR}/MAPL_Base ${esma_etc}/MAPL)
+
   add_custom_command (
     OUTPUT ${module}.F90
     COMMAND ${generator} ${module}Mod > ${module}.F90
@@ -11,5 +13,6 @@ macro (esma_create_stub_component srcs module)
     COMMENT "Making component stub for ${module}Mod in ${module}.F90"
     )
   add_custom_target(stub_${module} DEPENDS ${module}.F90)
-endmacro ()
+
+endfunction ()
 
