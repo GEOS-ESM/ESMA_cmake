fc67b8493a394f72f160f83ce436713748006c43
Matthew Thompson <matthew.thompson@nasa.gov>
2020-06-09 15:01:24 -0400

    Move back to a macro and change name of generator

--- esma_create_stub_component.cmake
+++ esma_create_stub_component.cmake
@@ -1,18 +1,18 @@
-function (esma_create_stub_component srcs module)
+macro (esma_create_stub_component srcs module)
   list (APPEND ${srcs} ${module}.F90)
 
-  find_file (generator
+  find_file (stub_generator
     NAME mapl_stub.pl
     PATHS ${MAPL_SOURCE_DIR}/MAPL_Base ${esma_etc}/MAPL)
 
   add_custom_command (
     OUTPUT ${module}.F90
-    COMMAND ${generator} ${module}Mod > ${module}.F90
-    MAIN_DEPENDENCY ${generator}
+    COMMAND ${stub_generator} ${module}Mod > ${module}.F90
+    MAIN_DEPENDENCY ${stub_generator}
     WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
     COMMENT "Making component stub for ${module}Mod in ${module}.F90"
     )
   add_custom_target(stub_${module} DEPENDS ${module}.F90)
 
-endfunction ()
+endmacro ()
 
