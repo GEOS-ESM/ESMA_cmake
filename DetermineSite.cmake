7298dd3736aaf4598e31e2b8cbfbc4d4230cb184
Matthew Thompson <matthew.thompson@nasa.gov>
2020-07-24 13:59:25 -0400

    Add ability to detect AWS

--- DetermineSite.cmake
+++ DetermineSite.cmake
@@ -11,7 +11,21 @@ elseif (${BUILD_SITE} MATCHES "pfe" OR ${BUILD_SITE} MATCHES "r[0-9]*i[0-9]*n[0-
 elseif (EXISTS /ford1/share/gmao_SIteam AND EXISTS /ford1/local AND ${CMAKE_SYSTEM_NAME} MATCHES "Linux")
    set (DETECTED_SITE "GMAO.desktop")
 else ()
-   set (DETECTED_SITE ${BUILD_SITE})
+   find_package(CURL)
+   if (CURL_FOUND)
+      execute_process(
+         COMMAND curl --connect-timeout 0.1 http://169.254.169.254/latest/meta-data/instance-id
+         TIMEOUT 1.0
+         RESULT_VARIABLE STATUS
+         )
+      if(STATUS AND NOT STATUS EQUAL 0)
+         set (DETECTED_SITE ${BUILD_SITE})
+      else ()
+         set (DETECTED_SITE "AWS")
+      endif ()
+   else ()
+      set (DETECTED_SITE ${BUILD_SITE})
+   endif ()
 endif ()
 
 set(GEOS_SITE ${DETECTED_SITE} CACHE STRING "Detected site for use with GEOS setup scripts")
