919ec4ca7de7d850bb95ac62b2a419fce0e47332
Amidu Oloso <amidu.o.oloso@nasa.gov>
2021-04-22 16:09:50 -0400

    Changed esmf_fullylinked to esmf in FindBaselibs.cmake

--- FindBaselibs.cmake
+++ FindBaselibs.cmake
@@ -99,7 +99,7 @@ if (Baselibs_FOUND)
      set (ESMF_LIBRARY ${ESMFDIR}/lib/libesmf.a)
      set (ESMF_LIBRARY_PATH ${ESMF_LIBRARY})
   else ()
-     set (ESMF_LIBRARY esmf_fullylinked)
+     set (ESMF_LIBRARY esmf)
      set (ESMF_LIBRARY_PATH ${ESMFDIR}/lib/lib${ESMF_LIBRARY}.so)
   endif ()
 
