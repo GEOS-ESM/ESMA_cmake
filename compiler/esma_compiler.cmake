314a31164c5f25cacbd1c97e3667c52f939ba35b
Matthew Thompson <matthew.thompson@nasa.gov>
2021-09-16 14:26:53 -0400

    Change flag files to have _Fortran

--- compiler/esma_compiler.cmake
+++ compiler/esma_compiler.cmake
@@ -1,9 +1,9 @@
 # CMake code involving compilers
 
-## Checks for support of flags
+## Checks for Fortran support
 list (APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/checks")
 include(check_fortran_support)
 
-## Files with options for compilers
-list (APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/options")
-include ("${CMAKE_Fortran_COMPILER_ID}")
+## Files with flags for Fortran compilers
+list (APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/flags")
+include ("${CMAKE_Fortran_COMPILER_ID}_Fortran")
