9aeb0a29f982ee31d91c555ada7db17878b23221
Matthew Thompson <matthew.thompson@nasa.gov>
2023-10-25 15:13:19 -0400

    Remove message

--- compiler/esma_compiler.cmake
+++ compiler/esma_compiler.cmake
@@ -12,8 +12,6 @@ include(check_fortran_support)
 list (APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/flags")
 include ("${CMAKE_Fortran_COMPILER_ID}_Fortran")
 
-message(STATUS "CMAKE_C_COMPILER_ID: ${CMAKE_C_COMPILER_ID}")
-
 ## We need to append some extra flags if using icx (aka CMAKE_C_COMPILER_ID=IntelLLVM)
 if (CMAKE_C_COMPILER_ID STREQUAL "IntelLLVM")
   set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wno-implicit-int")
