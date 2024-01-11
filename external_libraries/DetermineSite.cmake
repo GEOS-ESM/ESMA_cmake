3c26c4c615633941b924ce691651da981c8fc3ba
Matthew Thompson <matthew.thompson@nasa.gov>
2024-01-11 12:01:26 -0500

    Updates for v10.25.1 for SLES15

--- external_libraries/DetermineSite.cmake
+++ external_libraries/DetermineSite.cmake
@@ -11,15 +11,23 @@ set(DETECTED_SITE "UNKNOWN")
 
 if (${BUILD_SITE} MATCHES "discover*" OR ${BUILD_SITE} MATCHES "borg*" OR ${BUILD_SITE} MATCHES "warp*")
   set (DETECTED_SITE "NCCS")
-# At NAS, if you want to run on Rome, you have to build on Rome due to
-# OS differences. So we make a variable that can let us make the setup
-# scripts aware of this.
-elseif (${BUILD_SITE} MATCHES "pfe" OR ${BUILD_SITE} MATCHES "r[0-9]*i[0-9]*n[0-9]*")
+  # NCCS now has two OSs. We need to detect if we are on SLES 15. If so, we set a flag "BUILT_ON_SLES15"
+  # which we will use to make sure people building on SLES15 run on SLES15
+  # The commmand we use in bash is:
+  #   grep VERSION_ID /etc/os-release | cut -d= -f2 | cut -d. -f1 | sed 's/"//g'
+  execute_process(
+    COMMAND grep VERSION_ID /etc/os-release
+    COMMAND cut -d= -f2
+    COMMAND cut -d. -f1
+    COMMAND sed s/\"//g
+    OUTPUT_VARIABLE OS_RELEASE
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+  if (OS_RELEASE STREQUAL "15")
+    set (BUILT_ON_SLES15 TRUE)
+  endif ()
+elseif (${BUILD_SITE} MATCHES "pfe" OR ${BUILD_SITE} MATCHES "r[0-9]*i[0-9]*n[0-9]*" OR ${BUILD_SITE} MATCHES "r[0-9]*c[0-9]*t[0-9]*n[0-9]*")
   set (DETECTED_SITE "NAS")
-  set (BUILT_ON_ROME FALSE)
-elseif (${BUILD_SITE} MATCHES "r[0-9]*c[0-9]*t[0-9]*n[0-9]*")
-  set (DETECTED_SITE "NAS")
-  set (BUILT_ON_ROME TRUE)
 elseif (EXISTS /ford1/share/gmao_SIteam AND EXISTS /ford1/local AND ${CMAKE_SYSTEM_NAME} MATCHES "Linux")
   set (DETECTED_SITE "GMAO.desktop")
 endif ()
@@ -71,12 +79,11 @@ endif ()
 set(GEOS_SITE ${DETECTED_SITE} CACHE STRING "Detected site for use with GEOS setup scripts")
 message(STATUS "Setting GEOS_SITE to ${GEOS_SITE}")
 
-# Add message to say where we built at NAS
-if (DETECTED_SITE STREQUAL "NAS")
-  if (BUILT_ON_ROME)
-    message(STATUS "Building on AMD Rome nodes at NAS")
-  else()
-    message(STATUS "Building on Intel nodes at NAS")
- endif()
-endif()
+if (DETECTED_SITE STREQUAL "NCCS")
+  if (BUILT_ON_SLES15)
+    message(STATUS "Building on SLES15 at NCCS. Can only run on Milan processors")
+  else ()
+    message(STATUS "Building on SLES12 at NCCS. Can run on Cascade Lake or Skylake processors")
+  endif ()
+endif ()
 
