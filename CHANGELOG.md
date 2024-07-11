be2dd9deb3ab014be9de0d6ac899725291798699
Matthew Thompson <matthew.thompson@nasa.gov>
2024-07-11 11:50:58 -0400

    v3: Update the file esma_add_fortran_submodules.cmake

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -17,6 +17,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Deprecated
 
+## [3.47.0] - 2024-07-11
+
+### Fixed
+
+- Add more features to the file `esma_add_fortran_submodules.cmake` so that the function `esma_add_fortran_submodules` can handle several level of subdirectories.
+
 ## [3.46.0] - 2024-06-21
 
 ### Added
--- esma_support/esma_add_fortran_submodules.cmake
+++ esma_support/esma_add_fortran_submodules.cmake
@@ -14,14 +14,20 @@ function(esma_add_fortran_submodules)
 
     set(input ${CMAKE_CURRENT_SOURCE_DIR}/${ARG_SUBDIRECTORY}/${file})
     set(output ${CMAKE_CURRENT_BINARY_DIR}/${ARG_SUBDIRECTORY}_${file})
+    set(esma_internal "esma_internal_${ARG_SUBDIRECTORY}_${file}")
+
     add_custom_command(
       OUTPUT ${output}
       COMMAND ${CMAKE_COMMAND} -E copy ${input} ${output}
       DEPENDS ${input}
     )
-    set_property(SOURCE ${output} PROPERTY GENERATED 1)
+    add_custom_target(${esma_internal}  DEPENDS ${output})
+
+    set_property(SOURCE ${output} TARGET_DIRECTORY ${ARG_TARGET} PROPERTY GENERATED 1)
+    add_dependencies(${ARG_TARGET} ${esma_internal})
     target_sources(${ARG_TARGET} PRIVATE ${output})
 
   endforeach()
 
 endfunction()
+
