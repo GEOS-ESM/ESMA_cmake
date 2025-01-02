48ed2709550bd3733b7b9035a094ba866f03d576
Matthew Thompson <matthew.thompson@nasa.gov>
2025-01-02 11:58:58 -0500

    v4: Use LOCATION Python FIND_STRATEGY

--- python/esma_find_python3_module.cmake
+++ python/esma_find_python3_module.cmake
@@ -5,7 +5,6 @@
 # Citation: https://cmake.org/pipermail/cmake/2011-January/041666.html
 # Mark Moll
 
-set(Python3_FIND_STRATEGY VERSION)
 find_package(Python3 COMPONENTS Interpreter)
 
 function(esma_find_python3_module module)
