d67aead0eafe7384f9c57e29664b5ba0823b31cd
Matthew Thompson <matthew.thompson@nasa.gov>
2025-04-08 14:36:49 -0400

    Stuff

--- esma_support/esma_mepo_style.cmake
+++ esma_support/esma_mepo_style.cmake
@@ -15,18 +15,18 @@ function (esma_mepo_style raw_name mepo_name)
     if (IS_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/${path}${subdir}")
       set(${mepo_name} ${path}${subdir} PARENT_SCOPE)
       if (ARGS_FOUND)
-        set(${ARGS_FOUND} TRUE PARENT_SCOPE)
+	set(${ARGS_FOUND} TRUE PARENT_SCOPE)
       endif ()
       return()
     endif ()
+
   endforeach ()
 
   # Failed if got to here
   if (ARGS_FOUND)
     set(${ARGS_FOUND} FALSE PARENT_SCOPE)
   else ()
-    #ecbuild_error("Directory not found ${dir} (possibly sparse checkout)")
-    message(STATUS "Directory not found ${dir} (possibly sparse checkout)")
+    ecbuild_error("Directory not found ${dir} (possibly sparse checkout)")
   endif ()
 endfunction ()
 
