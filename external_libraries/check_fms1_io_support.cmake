eb6f7a6a6075153e2c3b127d79a2ef2effef1de9
Matthew Thompson <matthew.thompson@nasa.gov>
2025-07-03 09:50:25 -0400

    Updated check code

--- external_libraries/check_fms1_io_support.cmake
+++ external_libraries/check_fms1_io_support.cmake
@@ -1,29 +1,48 @@
 # At the top of your check_fms1_io_support.cmake file, BEFORE the macro definition
 set(_CHECK_FMS1_IO_SUPPORT_DIR "${CMAKE_CURRENT_LIST_DIR}")
 
-# Test if FMS library supports the deprecated FMS1 IO interface
 macro(check_fms1_io_support result_var)
-
-  # Use the captured directory
-  set(TEST_FILE_PATH "${_CHECK_FMS1_IO_SUPPORT_DIR}/test_fms1_io.f90")
-  message(DEBUG "Checking for FMS1 IO support in FMS library using test file: ${TEST_FILE_PATH}")
-
   # Get include directories from the FMS target
   get_target_property(FMS_INCLUDE_DIRS FMS::fms_r4 INTERFACE_INCLUDE_DIRECTORIES)
-  get_target_property(FMS_LINK_LIBRARIES FMS::fms_r4 INTERFACE_LINK_LIBRARIES)
+  
+  # Handle cases where properties might not be set
+  if(NOT FMS_INCLUDE_DIRS)
+    set(FMS_INCLUDE_DIRS "")
+  endif()
 
-  # Try to compile the tesy
+  # Set the test file path
+  set(TEST_FILE_PATH "${_CHECK_FMS1_IO_SUPPORT_DIR}/test_fms1_io.f90")
+  if(NOT EXISTS "${TEST_FILE_PATH}")
+    message(FATAL_ERROR "FMS1 IO test file not found: ${TEST_FILE_PATH}")
+  endif()
+
+  # Save the current value
+  set(_SAVED_CMAKE_TRY_COMPILE_TARGET_TYPE ${CMAKE_TRY_COMPILE_TARGET_TYPE})
+  
+  # Set to static library (compile only, no linking)
+  set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
+  # Build CMAKE_FLAGS
+  set(CMAKE_FLAGS_LIST "")
+  if(FMS_INCLUDE_DIRS)
+    list(APPEND CMAKE_FLAGS_LIST "-DINCLUDE_DIRECTORIES=${FMS_INCLUDE_DIRS}")
+  endif()
+
+  # Try to compile the test
   try_compile(${result_var}
     ${CMAKE_BINARY_DIR}/test_fms1_compile
-    SOURCES "${TEST_FILE_PATH}"
-    CMAKE_FLAGS "-DINCLUDE_DIRECTORIES=${FMS_INCLUDE_DIRS} -DLINK_LIBRARIES=${FMS_LINK_LIBRARIES}"
+    SOURCES ${TEST_FILE_PATH}
+    CMAKE_FLAGS ${CMAKE_FLAGS_LIST}
+    OUTPUT_VARIABLE COMPILE_OUTPUT
   )
+
+  # Restore the previous value
+  set(CMAKE_TRY_COMPILE_TARGET_TYPE ${_SAVED_CMAKE_TRY_COMPILE_TARGET_TYPE})
 
   if(${result_var})
     message(STATUS "FMS1 IO support is available.")
   else()
     message(STATUS "FMS1 IO support is NOT available.")
+    message(STATUS "Compile output: ${COMPILE_OUTPUT}")
   endif()
-
 endmacro()
-
--- external_libraries/test_fms1_io.f90
+++ external_libraries/test_fms1_io.f90
@@ -1,6 +1,6 @@
-program test_fms1_io
+subroutine test_fms1_io
     use fms_mod, only: file_exist
     implicit none
     logical :: exists
     exists = file_exist('dummy_file')
-end program test_fms1_io
+end subroutine test_fms1_io
