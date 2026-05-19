a05b98304bdece6b5a8adf368c20386f2284be61
Matthew Thompson <matthew.thompson@nasa.gov>
2026-05-19 12:25:38 -0400

    v4: Use copy_if_different for submodules

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -17,6 +17,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Deprecated
 
+## [4.38.0] - 2026-05-19
+
+### Changed
+
+- Use `copy_if_different` in `esma_add_fortran_submodules.cmake` to prevent unnecessary copying of submodule files when they haven't changed.
+
 ## [4.37.0] - 2026-04-13
 
 ### Added
--- esma_support/esma_add_fortran_submodules.cmake
+++ esma_support/esma_add_fortran_submodules.cmake
@@ -12,16 +12,16 @@ function(esma_add_fortran_submodules)
 
   foreach(file ${ARG_SOURCES})
 
-    set(input ${CMAKE_CURRENT_SOURCE_DIR}/${ARG_SUBDIRECTORY}/${file})
+    set(input  ${CMAKE_CURRENT_SOURCE_DIR}/${ARG_SUBDIRECTORY}/${file})
     set(output ${CMAKE_CURRENT_BINARY_DIR}/${ARG_SUBDIRECTORY}_SMOD_${file})
     set(esma_internal "esma_internal_${ARG_SUBDIRECTORY}_SMOD_${file}")
 
     add_custom_command(
       OUTPUT ${output}
-      COMMAND ${CMAKE_COMMAND} -E copy ${input} ${output}
+      COMMAND ${CMAKE_COMMAND} -E copy_if_different ${input} ${output}
       DEPENDS ${input}
     )
-    add_custom_target(${esma_internal}  DEPENDS ${output})
+    add_custom_target(${esma_internal} DEPENDS ${output})
 
     set_property(SOURCE ${output} TARGET_DIRECTORY ${ARG_TARGET} PROPERTY GENERATED 1)
     add_dependencies(${ARG_TARGET} ${esma_internal})
